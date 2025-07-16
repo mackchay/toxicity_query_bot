3343801517b33fdc160bfcfc69aac1e15a200a01
mackchay <56344123+mackchay@users.noreply.github.com>
2025-07-17 02:29:35 +0700

    Im tired boss

--- bot_local.py
+++ bot_local.py
@@ -116,7 +116,7 @@ def get_codellama_instruct_prompt(queries: list[str]) -> str:
         "If no correction is needed, write 'the query does not require corrections and optimization'."
     )
     message = "SQL queries:\n" + "\n".join(queries)
-    return f'<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{message} [/INST]'
+    return f'<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{message} [/INST] </s>'
 
 async def process_sql_with_llm(sql_file, llm_name, message=None, quantization=None):
     queries = read_sql_queries_from_csv(sql_file)
